27f21022f351d70aeeb4855105d69a3633ff54bd
Ermaotie <Meise1015@gmail.com>
2020-05-21 13:02:39 +0800

    Update msg.py

--- msg.py
+++ msg.py
@@ -1,9 +1,9 @@
 import requests
 import json
-SCKEY = "SCU97223Tb71ed152160cd8b2841a256675dc8ba35eb6d3db532c1"+".send?"
-root = "https://sc.ftqq.com/"+SCKEY+"text="
+SCKEY = "SCU97223Tb71ed152160cd8b2841a26675dc8ba35eb6d3db532c1"
+root = "https://sc.ftqq.com/"+SCKEY+".send?"+"text="
 try:
-    desp = open("weibo/处处吻--/7264541047.json","r",encoding='utf-8')
+    desp = open("weibo/微博用户名/微博id.json","r",encoding='utf-8')
     k = desp.read()
     desp.close()
     dic = json.loads(k)
